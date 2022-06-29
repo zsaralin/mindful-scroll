1baa8d2fb33243975f0883ed8ed27688aa9e580c
Saralin Zassman <zsaralin@gmail.com>
2022-06-29 14:53:50 -0400

    My first commit

--- src/App.js
+++ src/App.js
@@ -3,26 +3,29 @@
 import './App.css';
 import {useEffect, useRef, useState} from "react";
 import {Helmet} from "react-helmet";
-import { mul, EdgeShape, tilingTypes, IsohedralTiling }
+import {mul, EdgeShape, tilingTypes, IsohedralTiling}
     from './lib/tactile.js';
-import tiling from './Tiling.js'
+import tilingObject from './Tiling.js'
 
 function getRandomColor() {
     var o = Math.round, r = Math.random, s = 255;
-    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + 1 + ')';
+    return 'rgba(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ',' + 1 + ')';
 }
 
 function App() {
     const canvas = useRef();
     let color = getRandomColor();
+    const [randTiling, setRandTiling] = useState(null)
+    const [randEdges, setRandEdges] = useState(null)
 
     // disable right clicking
     document.oncontextmenu = function () {
         return false;
     }
     useEffect(() => {
-        redrawCanvas();
-        // tiling.drawRandomTiling()
+        const {tiling, edges} = tilingObject.makeRandomTiling()
+        setRandTiling(tiling)
+        setRandEdges(edges)
 
     }, []);
 
@@ -39,24 +42,40 @@ function App() {
     let offsetX = 0;
     let offsetY = 0;
 
+    useEffect(() => {
+        redrawCanvas();
+        pageScroll()
+    }, [randTiling]);
+
+    // useEffect(() => {
+    //     pageScroll()
+    // }, );
+
     // convert coordinates
     function toScreenX(xTrue) {
         return (xTrue + offsetX)
     }
+
     function toScreenY(yTrue) {
         return (yTrue + offsetY)
     }
+
     function toTrueX(xScreen) {
         return (xScreen) - offsetX;
     }
+
     function toTrueY(yScreen) {
         return (yScreen) - offsetY;
     }
-    function redrawCanvas() {
-        const canvas = document.querySelector('#canvas')
-        // setColor(getRandomColor())
 
-        // const canvas = document.getElementById("canvas");
+    function redrawCanvas() {
+        if (randTiling !== null && randEdges !== null) {
+            tilingObject.drawTiling(offsetX, offsetY, randTiling, randEdges);
+        }
+        // const canvas = document.querySelector('#canvas')
+        // setColor(getRandomColor())
+        const canvas = document.getElementById("canvas");
+
         const context = canvas.getContext("2d");
 
         // set the canvas to the size of the window
@@ -73,7 +92,9 @@ function App() {
         const canvas2 = document.getElementById("tiling-canvas")
         canvas2.width = document.body.clientWidth;
         canvas2.height = window.innerHeight;
-        tiling.drawRandomTiling()
+        if (randTiling && randEdges) {
+            tilingObject.drawTiling(offsetX, offsetY, randTiling, randEdges);
+        }
     }
 
     // if the window changes size, redraw the canvas
@@ -84,6 +105,7 @@ function App() {
     // mouse functions
     let leftMouseDown = false;
     let rightMouseDown = false;
+
     function onMouseDown(event) {
         // detect left clicks
         if (event.button == 0) {
@@ -104,6 +126,7 @@ function App() {
         prevCursorX = event.pageX;
         prevCursorY = event.pageY;
     }
+
     function onMouseMove(event) {
         // get mouse position
         cursorX = event.pageX;
@@ -134,6 +157,7 @@ function App() {
         prevCursorX = cursorX;
         prevCursorY = cursorY;
     }
+
     function onMouseUp() {
         // setColor(getRandomColor)
         leftMouseDown = false;
@@ -153,6 +177,7 @@ function App() {
     const prevTouches = [null, null]; // up to 2 touches
     let singleTouch = false;
     let doubleTouch = false;
+
     function onTouchStart(event) {
         if (event.touches.length == 1) {
             singleTouch = true;
@@ -169,6 +194,7 @@ function App() {
         prevTouches[1] = event.touches[1];
 
     }
+
     function onTouchMove(event) {
         // get first touch coordinates
         const touch0X = event.touches[0].pageX;
@@ -200,76 +226,80 @@ function App() {
         prevTouches[0] = event.touches[0];
         prevTouches[1] = event.touches[1];
     }
+
     function onTouchEnd(event) {
         singleTouch = false;
         doubleTouch = false;
     }
 
-    function drawLineEffect(context, x1, y1, color){
+    function drawLineEffect(context, x1, y1, color) {
         context.strokeStyle = color;
         context.lineWidth = 5;
         context.lineTo(x1, y1);
         context.stroke();
 
-        context.strokeStyle = color.substring(0,color.length-2)+ '0.7)';
+        context.strokeStyle = color.substring(0, color.length - 2) + '0.7)';
         context.lineWidth = 10;
         context.lineTo(x1, y1);
         context.stroke();
 
-        context.strokeStyle = color.substring(0,color.length-2)+ '0.5)';
+        context.strokeStyle = color.substring(0, color.length - 2) + '0.5)';
         context.lineWidth = 20;
         context.lineTo(x1, y1);
         context.stroke();
 
-        context.strokeStyle = color.substring(0,color.length-2)+ '0.3)';
+        context.strokeStyle = color.substring(0, color.length - 2) + '0.3)';
         context.lineWidth = 30;
         context.lineTo(x1, y1);
         context.stroke();
 
-        context.strokeStyle = color.substring(0,color.length-2)+ '0.2)';
+        context.strokeStyle = color.substring(0, color.length - 2) + '0.2)';
         context.lineWidth = 40;
         context.lineTo(x1, y1);
         context.stroke();
 
-        context.strokeStyle = color.substring(0,color.length-2)+ '0.1)';
+        context.strokeStyle = color.substring(0, color.length - 2) + '0.1)';
         context.lineWidth = 50;
         context.lineTo(x1, y1);
         context.stroke();
     }
-  return (
-    <div className="App">
-        <Helmet>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-        </Helmet>
-        <style>
-            {`
+
+    function pageScroll() {
+        offsetY += 1
+        redrawCanvas()
+        setTimeout(pageScroll,50);
+    }
+
+
+    return (
+        <div className="App">
+            <Helmet>
+                <meta name="viewport"
+                      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+            </Helmet>
+            <style>
+                {`
           #canvas {
           }
         `}
-        </style>
-        <div class = "wrapper">
-      <canvas ref={canvas} id = "canvas"
-              // onMouseDown={onMouseDown}
-              // onMouseUp={onMouseUp}
-              // onMouseOut={onMouseUp}
-              // onMouseMove={onMouseMove}
-              // onTouchStart={onTouchStart}
-              // onTouchEnd={onTouchEnd}
-              // onTouchCancel={onTouchEnd}
-              // onTouchMove={onTouchMove}
-      >Your browser does not support HTML5 canvas</canvas>
-        <canvas id = "tiling-canvas" width = "document.body.clientWidth" height = "client.innerHeight"
-                onMouseDown={onMouseDown}
-                onMouseUp={onMouseUp}
-                onMouseOut={onMouseUp}
-                onMouseMove={onMouseMove}
-                onTouchStart={onTouchStart}
-                onTouchEnd={onTouchEnd}
-                onTouchCancel={onTouchEnd}
-                onTouchMove={onTouchMove}
-        ></canvas>
+            </style>
+            <div className="wrapper">
+                <canvas ref={canvas} id="canvas"
+                >Your browser does not support HTML5 canvas
+                </canvas>
+                <canvas id="tiling-canvas" width="document.body.clientWidth" height="client.innerHeight"
+                        onMouseDown={onMouseDown}
+                        onMouseUp={onMouseUp}
+                        onMouseOut={onMouseUp}
+                        onMouseMove={onMouseMove}
+                        onTouchStart={onTouchStart}
+                        onTouchEnd={onTouchEnd}
+                        onTouchCancel={onTouchEnd}
+                        onTouchMove={onTouchMove}
+                ></canvas>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
+
 export default App;
--- src/Tiling.js
+++ src/Tiling.js
@@ -21,8 +21,8 @@ class Tiling{
             ctx.lineWidth = 8.0;
             ctx.strokeStyle = '#000';
             // Define a world-to-screen transformation matrix that scales by 50x.
-            const ST = [50.0, 0.0, 0.0,
-                0.0, 70.0, 0.0];
+            const ST = [80.0, 0.0, 0.0,
+                0.0, 90.0, 0.0];
 
             for (let i of tiling.fillRegionBounds(-2, -2, 12, 12)) {
                 const T = mul(ST, i.T);
@@ -69,6 +69,72 @@ class Tiling{
         }
     }
 
+    drawTiling(offsetX, offsetY, tiling, edges) {
+        {
+            var canvas = document.getElementById('tiling-canvas');
+            var ctx = canvas.getContext('2d');
+            // const {tiling, edges} = this.makeTiling();
+
+            // Make some random colours.
+            // let cols = [];
+            // for (let i = 0; i < 3; ++i) {
+            //     cols.push('rgb(' +
+            //         Math.floor(Math.random() * 255.0) + ',' +
+            //         Math.floor(Math.random() * 255.0) + ',' +
+            //         Math.floor(Math.random() * 255.0) + ')');
+            // }
+            //
+            ctx.lineWidth = 8.0;
+            ctx.strokeStyle = '#000';
+            // Define a world-to-screen transformation matrix that scales by 50x.
+            const ST = [80.0, 0, 0.0,
+                0.0, 90.0, 0.0];
+
+            for (let i of tiling.fillRegionBounds(-2, -2, 12, 12)) {
+                const T = mul(ST, i.T);
+                // ctx.fillStyle = cols[ tiling.getColour( i.t1, i.t2, i.aspect ) ];
+                ctx.fillStyle = "rgba(255, 255, 255, 0)"
+                let start = true;
+                ctx.beginPath();
+
+                for (let si of tiling.shape()) {
+                    const S = mul(T, si.T);
+                    let seg = [mul(S, {x: 0.0 , y: 0.0})];
+
+                    if (si.shape != EdgeShape.I) {
+                        const ej = edges[si.id];
+                        seg.push(mul(S, ej[0]));
+                        seg.push(mul(S, ej[1]));
+                    }
+
+                    seg.push(mul(S, {x: 1.0, y: 0.0}));
+
+                    if (si.rev) {
+                        seg = seg.reverse();
+                    }
+
+                    if (start) {
+                        start = false;
+                        ctx.moveTo(seg[0].x + offsetX, seg[0].y + offsetY);
+                    }
+
+                    if (seg.length == 2) {
+                        ctx.lineTo(seg[1].x + offsetX, seg[1].y + offsetY);
+                    } else {
+                        ctx.bezierCurveTo(
+                            seg[1].x + offsetX, seg[1].y + offsetY,
+                            seg[2].x + offsetX, seg[2].y + offsetY,
+                            seg[3].x + offsetX, seg[3].y + offsetY);
+                    }
+                }
+
+                ctx.closePath();
+                ctx.fill();
+                ctx.stroke();
+            }
+        }
+    }
+
     makeRandomTiling() {
         // Construct a tiling
         const tp = tilingTypes[Math.floor(81 * Math.random())];
@@ -100,6 +166,46 @@ class Tiling{
                 ej.push({x: 1.0 - ej[0].x, y: -ej[0].y});
             } else if (shp == EdgeShape.U) {
                 ej.push({x: Math.random() * 0.6, y: Math.random() - 0.5});
+                ej.push({x: 1.0 - ej[0].x, y: ej[0].y});
+            }
+
+            edges.push(ej);
+        }
+
+        return {tiling: tiling, edges: edges}
+    }
+
+    makeTiling() {
+        // Construct a tiling
+        const tp = tilingTypes[Math.floor(1)];
+        let tiling = new IsohedralTiling(tp);
+
+        // Randomize the tiling vertex parameters
+        let ps = tiling.getParameters();
+        for (let i = 0; i < ps.length; ++i) {
+            ps[i] += 1 * 0.1 - 0.05;
+        }
+        tiling.setParameters(ps);
+
+        // Make some random edge shapes.  Note that here, we sidestep the
+        // potential complexity of using .shape() vs. .parts() by checking
+        // ahead of time what the intrinsic edge shape is and building
+        // Bezier control points that have all necessary symmetries.
+
+        let edges = [];
+        for (let i = 0; i < tiling.numEdgeShapes(); ++i) {
+            let ej = [];
+            const shp = tiling.getEdgeShape(i);
+            if (shp == EdgeShape.I) {
+                // Pass
+            } else if (shp == EdgeShape.J) {
+                ej.push({x: 1 * 0.6, y: 1 - 0.5});
+                ej.push({x: 1 * 0.6 + 0.4, y: 1 - 0.5});
+            } else if (shp == EdgeShape.S) {
+                ej.push({x: 1 * 0.6, y: 1 - 0.5});
+                ej.push({x: 1.0 - ej[0].x, y: -ej[0].y});
+            } else if (shp == EdgeShape.U) {
+                ej.push({x: 1 * 0.6, y: 1 - 0.5});
                 ej.push({x: 1.0 - ej[0].x, y: ej[0].y});
             }
 
