17a579bf5e56ea6f126da6b8db325f75c3f039b0
Saralin Zassman <zsaralin@gmail.com>
2022-08-03 22:44:41 -0400

    My first commit

--- src/App.js
+++ src/App.js
@@ -208,9 +208,9 @@ function App() {
             const touch1X = event.touches[1].pageX;
             const touch1Y = event.touches[1].pageY;
             let colorCtx = document.getElementById('invis-canvas').getContext("2d");
-            // let s = colorCtx.getImageData(touch0X, touch0Y, 1, 1).data.toString().substring(0,5)
-            // console.log(s.substring(0,5))
-            if (colorCtx.getImageData(touch0X, touch0Y, 1, 1).data.toString().substring(0,5) !== '0,0,0'){
+            let s = colorCtx.getImageData(touch1X, touch1Y, 1, 1).data.toString().substring(0,5)
+            console.log(s !== '0,0,0')
+            if (colorCtx.getImageData(touch0X, touch0Y, 1, 1).data.toString().trim().substring(0,5) !== '0,0,0'){
                 removeLastPoint()
             }
             removeLastPoint()
